cb9c18156f58b46926bf257717d26e591d246666
carson-fn <180336862+carson-fn@users.noreply.github.com>
2026-03-13 12:38:52 -0600

    Initialize Project

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3000;
+
+app.use(
+    cors({
+        origin: ["http://localhost:5173"],
+    })
+)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the server!");
+})
+
+app.get("/api/hello", (req, res) => {
+    res.json({message: "Hello world!"});
+})
